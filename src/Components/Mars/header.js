72a97d3997704445ac3d46f7c4615dd7923316f2
Melody McClure <melodylayne79@gmail.com>
2022-06-15 18:49:42 -0500

    make correction in missing title element.

--- src/Components/Mars/header.js
+++ src/Components/Mars/header.js
@@ -8,7 +8,7 @@ export default function Header() {
             <h1>Welcome to the NASA Mars Rovers Image Search site!</h1>
             <p>
                 When completed, this page will allow you to search from any of the cameras on any of
-                the four Mars rovers in the Nasa Database using the NASA API. Currently, 25 random images from the 6th of June, 2022 are what you see.
+                the four Mars rovers in the Nasa Database using the NASA API. Currently, 25 random images from the Preseverance Rover on June 6th, 2022 are what you see.
             </p>
         </div>
     );

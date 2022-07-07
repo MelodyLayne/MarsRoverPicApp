803d38110fe21152652d387f543ef133aa8da597
Melody McClure <melodylayne79@gmail.com>
2022-07-07 16:39:34 -0500

    add build file

---
+++ build/precache-manifest.1914238f982887883bdc5a1c95475b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ff1fa5a39370f27232e9b5d64bac9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdd5d043e8b838f4d452",
+    "url": "/static/css/main.8d12a12b.chunk.css"
+  },
+  {
+    "revision": "bff2bab46d9140ae15ce",
+    "url": "/static/js/2.6eda3ff7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6eda3ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd5d043e8b838f4d452",
+    "url": "/static/js/main.181b1570.chunk.js"
+  },
+  {
+    "revision": "f976b5cbf1912567155a",
+    "url": "/static/js/runtime-main.67418e3c.js"
+  }
+]);
